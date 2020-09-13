55f702321a501dbbc0637a9d08562804b754dbe3
taibislamdipu <mailtaibislam@gmail.com>
2020-09-13 22:15:05 +0600

    all code done

--- src/App.js
+++ src/App.js
@@ -9,27 +9,45 @@ import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Book from './components/Book/Book';
 import Header from './components/Header/Header';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+
+export const UserContext = createContext();
 
 function App() {
+
+  const [loggedInUser, setLoggedInUser] = useState({});
+
   return (
-      <Router>
-          <Header/>
-          <Switch>
-            <Route path="/home">
-              <Home />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/book/:bedType">
-              <Book />
-            </Route>
-            <Route exact path="/">
-              <Home />
-            </Route>
-          </Switch>
-      </Router>
+
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <p>Name: {loggedInUser.name} </p>
+
+    <Router>
+        <Header />
+      <Switch>
+
+        <Route path="/home">
+          <Home />
+        </Route>
+
+        <Route path="/login">
+          <Login />
+        </Route>
+
+          <PrivateRoute path="/book/:bedType">
+          <Book />
+          </PrivateRoute>
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+      </Switch>
+    </Router>
+    </UserContext.Provider>
   );
 }
+
+
 
 export default App;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,59 @@
 import React, { useContext } from 'react';
 
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { UserContext } from '../../App';
+import { useHistory, useLocation } from 'react-router-dom';
+
 
 const Login = () => {
+
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+    const history = useHistory();
+    const location = useLocation()
+    const { from } = location.state || { from: { pathname: "/" } };
+
+
+
+    const handleGoogleSignIn = () => {
+
+        if (firebase.apps.length === 0) {
+            // Initialize Firebase
+            firebase.initializeApp(firebaseConfig);
+        }
+
+        //Google sign-in provider
+        var provider = new firebase.auth.GoogleAuthProvider();
+
+        //Authenticate with Firebase using the Google provider object.
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+
+            var { displayName, email } = result.user;
+            const singnedInUser = { name: displayName, email }
+            setLoggedInUser(singnedInUser);
+            history.replace(from);
+
+        }).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+
+    }
     return (
         <div>
             <h1>This is Login</h1>
+            <button onClick={handleGoogleSignIn}>Google Sign in</button>
         </div>
     );
 };
