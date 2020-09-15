8b259b2bb81ea6c3a32d8ad7d74433ee02b27458
taibislamdipu <mailtaibislam@gmail.com>
2020-09-15 20:58:24 +0600

    _redirects file added in build folder

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -40,13 +40,9 @@ const Login = () => {
 
         }).catch(function (error) {
             // Handle Errors here.
-            var errorCode = error.code;
             var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
+            console.log(errorMessage);
+
         });
 
     }
